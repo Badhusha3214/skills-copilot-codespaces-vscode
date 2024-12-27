09dfb049e1079c83c2c1a1f1106bf0093a3302fd
Badhusha <53377403+Badhusha3214@users.noreply.github.com>
2024-12-27 07:53:26 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,11 @@
+function skillsMember(){
+    var skills = {
+        "HTML": "90%",
+        "CSS": "80%",
+        "JavaScript": "70%",
+        "React": "60%",
+        "NodeJS": "50%",
+        "MongoDB": "40%"
+    }
+    return skills;
+}
